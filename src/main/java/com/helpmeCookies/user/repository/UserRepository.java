fe44c7f09d2f691bc48977b2a19b0c4f4ca2aeec
yooonwodyd <dbswodyd00@naver.com>
2024-09-09 19:54:19 +0900

    #1 프로젝트 기본 설정

---
+++ src/main/java/com/helpmeCookies/user/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.helpmeCookies.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.helpmeCookies.user.entity.User;
